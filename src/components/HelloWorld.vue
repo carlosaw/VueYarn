7fffad736e90e41ae10ae4166cb0175afafd7656
carlosaw <carlosfreevg@gmail.com>
2020-12-03 21:56:55 -0400

    Tabela sem modal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{ titulo }}</h1>
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <td>#</td>
+            <td>Name</td>
+            <td>Address</td>
+            <td>Phone</td>
+            <td>E-mail</td>
+            <td>Ações</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="usuario in usuarios" v-bind:key="usuario.id">
+            <td>{{usuario.id}}</td>
+            <td>{{usuario.name}}</td>
+            <td>{{usuario.address}}</td>
+            <td>{{usuario.phone}}</td>
+            <td>{{usuario.email}}</td>
+            <td>
+              <button class="btn btn-info">Editar</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
   </div>
 </template>
 
@@ -35,6 +33,16 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  }, data () {
+    return {
+      titulo: "Tabela",
+      usuarios: [
+        {id:1, name:'Carlos', address:'New York', phone:36825291, email:'carlos@gmail.com'},
+        {id:2, name:'Ana Paula', address:'Rondonópolis', phone:36825291, email:'ana@gmail.com'},
+        {id:3, name:'Mara', address:'Várzea Grande', phone:36825291, email:'mara@gmail.com'},
+        {id:4, name:'Fulano', address:'Cuiabá', phone:36825291, email:'fulano@gmail.com'}
+      ]
+    }
   }
 }
 </script>
